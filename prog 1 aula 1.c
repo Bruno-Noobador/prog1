a27a748b85068e303ec4170b7d1f45def792bf60
Bruno Noobador <51370142+Bruno-Noobador@users.noreply.github.com>
2025-08-14 14:50:18 -0300

    churrasco

--- prog 1 aula 1.c
+++ prog 1 aula 1.c
@@ -3,10 +3,15 @@
 int main()
 {
     goto(açougue);
-    Produto picanha = comprar(picanha);
-    Carne carne = fatiar(picanha);
+    Produto picanha = comprar("picanha");
+    char* tipo_de_corte = "";
+    Carne carne = fatiar(picanha, tipo_de_corte);
+    
 
-    carne.cozinhar();
+    char* modo_de_cozer = "";
+    float s_tempo_de_cozer = 10 
+    carne.cozinhar(tempo_de_cozer, modo_de_cozer);
+
     carne.servir();
 
 }
